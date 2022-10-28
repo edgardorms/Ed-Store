5701843f562ce01dced81880ec763a186a4780b3
Edgardo Ramos <rhedgardo@gmail.com>
2022-10-28 12:35:23 -0300

    cambiado de api por json

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,8 @@
 function ProductCard({ product }) {
     return (
-        <div className="p-4 mt-6 w-80">
-              <div className="h-[32rem] rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-                <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src={product.images[0]} alt="blog"/>
+        <div className="p-4 mt-6 w-[360px]">
+              <div className="h-[512px] rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                <img className="h-48 w-full my-5 object-contain object-center scale-110 transition-all duration-400 hover:scale-100" src={product.images[0]} alt="blog"/>
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{product.category}</h2>
                   <h1 className="title-font text-lg font-medium text-gray-600 mb-3">{product.title}</h1>
