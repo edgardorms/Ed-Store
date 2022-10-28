6d47fc32dcc6edf92d1892124693313fbb8c56f4
Edgardo Ramos <rhedgardo@gmail.com>
2022-10-27 21:25:23 -0300

    chng color navbtn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
         <Link
           to="/"
           type="button"
-          className="border border-indigo-900 bg-indigo-500 text-white rounded-md px-4 py-2 m-8 transition duration-500 ease select-none hover:bg-rojo4 focus:outline-none focus:shadow-outline"
+          className="bg-gradient-to-r from-indigo-600 to-indigo-800 text-white hover:scale-105 drop-shadow-md  shadow-cla-blue rounded-md px-4 py-2 m-8 transition duration-500 ease select-none hover:bg-rojo4 focus:outline-none focus:shadow-outline"
         >
           {" "}
           Home
@@ -19,7 +19,7 @@ function Navbar() {
         <Link
           to="/productos"
           type="button"
-          className="border  border-indigo-900 bg-indigo-500 text-white rounded-md px-4 py-2 m-8 transition duration-500 ease select-none hover:bg-rojo4 focus:outline-none focus:shadow-outline"
+          className="bg-gradient-to-r  from-indigo-600 to-indigo-800 text-white hover:scale-105 drop-shadow-md  shadow-cla-blue rounded-md px-4 py-2 m-8 transition duration-500 ease select-none hover:bg-rojo4 focus:outline-none focus:shadow-outline"
         >
           {" "}
           Productos
